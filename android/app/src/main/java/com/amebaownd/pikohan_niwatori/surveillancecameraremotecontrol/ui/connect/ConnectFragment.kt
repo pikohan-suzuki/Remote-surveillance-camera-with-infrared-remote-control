702d44d4be35d086eeef41a79da618a5e3567e40
pikohan-suzuki <b1702996@planet.kanazawa-it.ac.jp>
2021-01-30 19:52:46 +0900

    Connect

--- android/app/src/main/java/com/amebaownd/pikohan_niwatori/surveillancecameraremotecontrol/ui/connect/ConnectFragment.kt
+++ android/app/src/main/java/com/amebaownd/pikohan_niwatori/surveillancecameraremotecontrol/ui/connect/ConnectFragment.kt
@@ -11,36 +11,44 @@ import android.view.View
 import android.view.ViewGroup
 
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.amebaownd.pikohan_niwatori.surveillancecameraremotecontrol.MainActivity
+import com.amebaownd.pikohan_niwatori.surveillancecameraremotecontrol.MainViewModel
+import com.amebaownd.pikohan_niwatori.surveillancecameraremotecontrol.data.Address
 
 import com.amebaownd.pikohan_niwatori.surveillancecameraremotecontrol.databinding.ConnectFragmentBinding
+import com.amebaownd.pikohan_niwatori.surveillancecameraremotecontrol.util.EventObserver
 import com.amebaownd.pikohan_niwatori.surveillancecameraremotecontrol.util.getViewModelFactory
 
-class ConnectFragment :Fragment(){
+class ConnectFragment : Fragment() {
 
-private val viewModel:ConnectViewModel by viewModels{getViewModelFactory()}
-private lateinit var mainFragmentBinding:ConnectFragmentBinding
+    private val viewModel: ConnectViewModel by viewModels { getViewModelFactory((this.activity as MainActivity).application) }
+    private lateinit var mainFragmentBinding: ConnectFragmentBinding
 
-        override fun onCreateView(
-        inflater:LayoutInflater,
-        container:ViewGroup?,
-        savedInstanceState:Bundle?
-        ):View{
-        mainFragmentBinding=ConnectFragmentBinding.inflate(inflater,container,false).apply{
-        viewModel=this@ConnectFragment.viewModel
-            lifecycleOwner=viewLifecycleOwner
-                    }
-                    return mainFragmentBinding.root
-                    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        mainFragmentBinding = ConnectFragmentBinding.inflate(inflater, container, false).apply {
+            viewModel = this@ConnectFragment.viewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
+        return mainFragmentBinding.root
+    }
 
-                    override fun onViewCreated(view:View,savedInstanceState:Bundle?){
-                    super.onViewCreated(view,savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-                    }
+        viewModel.connectEvent.observe(this.viewLifecycleOwner,EventObserver{
+            navigateToRemoteControlFragment(it)
+        })
+    }
 
-//    private fun navigateToAaaa(){
-//        val action = MainFragmentDirections
-//            .actionMainFragmentToRecordFragment()
-//        findNavController().navigate(action)
-//    }
+    private fun navigateToRemoteControlFragment(address: Address){
+        val action = ConnectFragmentDirections
+            .actionConnectFragmentToRemoteControlFragment(address.ipAddress,address.port)
+        findNavController().navigate(action)
+    }
 
-                    }
+}
--- android/app/src/main/java/com/amebaownd/pikohan_niwatori/surveillancecameraremotecontrol/ui/connect/ConnectViewModel.kt
+++ android/app/src/main/java/com/amebaownd/pikohan_niwatori/surveillancecameraremotecontrol/ui/connect/ConnectViewModel.kt
@@ -1,11 +1,27 @@
 package com.amebaownd.pikohan_niwatori.surveillancecameraremotecontrol.ui.connect
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.amebaownd.pikohan_niwatori.surveillancecameraremotecontrol.data.Address
 import com.amebaownd.pikohan_niwatori.surveillancecameraremotecontrol.repository.Repository
+import com.amebaownd.pikohan_niwatori.surveillancecameraremotecontrol.util.Event
+import com.amebaownd.pikohan_niwatori.surveillancecameraremotecontrol.util.isNotNullOrBlankOrEmpty
 
-class ConnectViewModel(private val repository:Repository):ViewModel(){
+class ConnectViewModel(private val repository: Repository) : ViewModel() {
 
-        fun start(){
 
+    var ipAddress = MutableLiveData<String>("192.168.10.10")
+    var port = MutableLiveData<String>("60010")
+    private var _connectEvent = MutableLiveData<Event<Address>>()
+    val connectEvent:LiveData<Event<Address>> = _connectEvent
+
+    fun onConnectButtonClicked(){
+        if(ipAddress.value.isNotNullOrBlankOrEmpty()){
+            if(ipAddress.value!!.split(".").size == 4){
+                val address = Address(ipAddress = ipAddress.value!!,port=port.value!!.toInt())
+                _connectEvent.value = Event(address)
+            }
         }
-        }
+    }
+}
